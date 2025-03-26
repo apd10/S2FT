1f4b4aa67e7d1acb37a5ff85f341928dc54dd47b
Aditya Desai <apd10@rice.edu>
2025-03-25 22:42:24 -0500

    smaple

---
+++ experiments/dext/sample.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+
+OUTPUT=$1
+ZERO_STAGE=$2
+if [ "$OUTPUT" == "" ]; then
+    OUTPUT=./outs/commonsense/lora_llama3
+fi
+if [ "$ZERO_STAGE" == "" ]; then
+    ZERO_STAGE=0                    # lets use zero_stage 1 for now
+fi
+mkdir -p $OUTPUT
+
+master_port=$((RANDOM % 5000 + 20000))
+# add offload add master_port if socket error
+deepspeed --include=localhost:2,3 \
+    --master_port $master_port ./train/finetune.py \
+    --model_name_or_path meta-llama/Meta-Llama-3-8B \
+    --per_device_train_batch_size 16 \
+    --per_device_eval_batch_size 16 \
+    --max_seq_len 2048 \
+    --learning_rate 5e-5 \
+    --weight_decay 0. \
+    --num_train_epochs 3 \
+    --dtype bf16 \
+    --gradient_accumulation_steps 1 \
+    --lr_scheduler_type linear \
+    --num_warmup_steps 100 \
+    --seed 0 \
+    --zero_stage $ZERO_STAGE \
+    --deepspeed \
+    --gradient_checkpointing \
+    --save_interval 5000 \
+    --instruction_type single \
+    --val_set_size 120 \
+    --eval_step 80 \
+    --dext \
+    --dext_config_file $PWD/dext/250M.json \
+    --data_path  $PWD/LLM-Adapters/ft-training_set/commonsense_170k.json  \
+    --eval_delay 0.5 \
+    --output_dir $OUTPUT 2> >(tee $OUTPUT/err.log >&2) | tee $OUTPUT/training.log \
